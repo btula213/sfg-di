b872cfef99e12b4e808143f9266c0b630bb1fe45
btu1a213 <94585860+btula213@users.noreply.github.com>
2021-11-19 18:16:40 -0800

    Using the profiles to change the behavior of the application. In the example, the sayHello will show the message from I18nEnglishGreetingService or I18nSpanishGreetingService based on the profile. # spring.profiles.active=ES If there is default profile defined, then if none of the profiles are set, then the default bean will be injected.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,10 +1,7 @@
 package guru.springframework.sfgdi;
 
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.PropertyInjectedController;
-import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,7 +13,17 @@ public class SfgDiApplication {
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
         /* Spring is providing the myController object for us
         *  This is the Dependency Injection
+        *  spring.profiles.active=ES
         * */
+
+        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+
+        /* Based on the active profile defined in the application.properties,
+        *  it will display the message from English or Spanish Bean
+        * */
+        System.out.println(" --- I18nController Bean");
+        System.out.println(i18nController.sayHello());
+
         MyController myController = (MyController) ctx.getBean("myController");
         System.out.println(" --- Primary Bean");
         System.out.println(myController.sayHello());
