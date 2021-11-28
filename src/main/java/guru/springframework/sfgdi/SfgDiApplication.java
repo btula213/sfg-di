32857b82f54e480e663ed27f5e36509b3555ce9d
btu1a213 <94585860+btula213@users.noreply.github.com>
2021-11-27 16:07:10 -0800

    Demostration of SingletonBean and PrototypeBean creation and number of instances SingletonBean created at the time of startup. Only 1 instance of Singleton. PrototypeBean created at the time of need, and each time it is called. Multiple instances of PrototypeBean. ConfigurableBeanFactory.SCOPE_PROTOTYPE is used to define PrototypeBean. However, SingletonBean need not be specified, as its the Spring's default type.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -3,6 +3,8 @@ package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.controllers.*;
 import guru.springframework.sfgdi.service.I18nEnglishGreetingService;
+import guru.springframework.sfgdi.service.PrototypeBean;
+import guru.springframework.sfgdi.service.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,9 +17,9 @@ public class SfgDiApplication {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
         /* Spring is providing the myController object for us
-        *  This is the Dependency Injection
-        *  spring.profiles.active=ES
-        * */
+         *  This is the Dependency Injection
+         *  spring.profiles.active=ES
+         * */
 
         I18nController i18nController = (I18nController) ctx.getBean("i18nController");
 
@@ -26,8 +28,8 @@ public class SfgDiApplication {
         System.out.println(petController.whichPetIsTheBest());
 
         /* Based on the active profile defined in the application.properties,
-        *  it will display the message from English or Spanish Bean
-        * */
+         *  it will display the message from English or Spanish Bean
+         * */
         System.out.println(" --- I18nController Bean");
         System.out.println(i18nController.sayHello());
 
@@ -35,7 +37,7 @@ public class SfgDiApplication {
         System.out.println(" --- Primary Bean");
         System.out.println(myController.sayHello());
         System.out.println(" --- Property");
-        PropertyInjectedController propertyInjectedController = (PropertyInjectedController)  ctx.getBean("propertyInjectedController");
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
         System.out.println(propertyInjectedController.greetingService.sayGreeting());
 
         System.out.println(" --- Setter Injected");
@@ -43,13 +45,21 @@ public class SfgDiApplication {
         System.out.println(setterInjectedController.getGreeting());
 
         System.out.println(" --- Contructor Injected");
-        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController)  ctx.getBean("constructorInjectedController");
+        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
         System.out.println(constructorInjectedController.getGreeting());
 
-        System.out.println(" 000 English Greeting Service 000");
+        System.out.println("------- Bean Scope ----------");
+        System.out.println("Singleton Bean is created only once at the startup. Check the creation log:\n======");
+        SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+        System.out.println("Singleton Bean 1: " + singletonBean1.getMyScope());
+        SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+        System.out.println("Singleton Bean 2: " + singletonBean2.getMyScope());
 
-
-
+        System.out.println("Prototype Bean is created each type an object is needed. Check the log below:\n======");
+        PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+        System.out.println("Prototype Bean 1: "+prototypeBean1.getMyScope());
+        PrototypeBean prototypeBean2  = ctx.getBean(PrototypeBean.class);
+        System.out.println("Prototype Bean 2: "+prototypeBean2.getMyScope());
     }
 
 }
